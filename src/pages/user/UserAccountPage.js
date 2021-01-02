2cab241c9bd1f0cd5ad338215ce815d02dc855f8
szilex <32484638+szilex@users.noreply.github.com>
2021-01-02 15:44:24 +0100

    Add user account page

--- src/pages/user/UserAccountPage.js
+++ src/pages/user/UserAccountPage.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
+import {Spinner} from 'react-bootstrap'
+import { useHistory } from 'react-router-dom';
+import UserService from '../../services/UserService'
 
 const UserAccountPage = () => {
-    return(
-        <div>
-            <h1>User account page</h1>
-        </div>
-    )
+    
+    const history = useHistory();
+    const [userData, setUserData] = useState({ fetched: false })
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await UserService.getCurrentUser()
+                console.log(result)
+                setUserData({ user: result, fetched: true })
+            } catch (exception) {
+                if (exception && exception.message && exception.message.includes(400)) {
+                    setUserData({ fetched: true, error: "No regular expenses found" })
+                } else {
+                    setUserData({ fetched: true, error: "Server error" })
+                }     
+            }
+        }
+        fetchData();
+    }, [])
+
+    if (!userData.fetched) {
+        return (
+            <div className="spinner">
+                <Spinner animation="border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </Spinner>  
+                <br/><br/>
+                <h2>Fetching content...</h2>  
+            </div>
+        )    
+    } else {
+        if (userData.error) {
+            return (
+                <>
+                    <h1>Error while retrieving user data: {userData.error}</h1>
+                    <button className="btn btn-primary btn-block" type="submit" onClick={() => history.goBack()}>Go back</button>
+                </>
+            )
+        } else {
+            return (
+                <>
+                <h1>Account details</h1>
+                <h3>Login: {userData.user.login}</h3>
+                <h3>First name: {userData.user.firstName}</h3>
+                <h3>Last name: {userData.user.lastName}</h3>
+                <button className="btn btn-primary btn-block" type="submit" onClick={() => history.goBack()}>Go back</button>
+                </>
+            )
+        }
+    }
 }
 
 export default UserAccountPage;
