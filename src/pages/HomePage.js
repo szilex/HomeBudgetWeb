b4cee897972797645c937874f3c4d61bb5d9cb90
szilex <32484638+szilex@users.noreply.github.com>
2021-01-03 19:34:11 +0100

    Fix logo color on main page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -17,7 +17,7 @@ class HomePage extends React.Component {
     render() {
         return (
             <>
-            <Image src={HomeLogo} alt="Home Budget icon" thumbnail style={{backgroundColor: '#0B0C10', borderColor: '#FFFFFF', color: '#45A293', fill: '#FFFFFF',  borderRadius: '50px', height: '40%', width: '40%', marginBottom: 20,  }}/>
+            <Image src={HomeLogo} alt="Home Budget icon" thumbnail style={{background: 'linear-gradient(to right, #c56400, #f7bf6a)', borderRadius: '50px', borderWidth: "2dp" , height: '35%', width: '35%', marginBottom: 20,  }}/>
             <h1>Welcome to Home Budget Web app</h1>
             </>
         )
